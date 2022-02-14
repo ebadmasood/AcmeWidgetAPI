9a32c07aa956bab4e97b0c24e01f84903b8ae9da
Ebad Masood <ebad.masood@gmail.com>
2022-02-13 21:01:03 -0500

    adding validation

--- Controllers/ActivitySignUpController.cs
+++ Controllers/ActivitySignUpController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AcmeWidgetAPI.Repositories;
+using AcmeWidgetAPI.Validation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -27,15 +28,27 @@ namespace AcmeWidgetAPI.Controllers
         public async Task<ActivitySignUpForm> GetForm(int id) => await _repository.Get(id);
 
         [HttpPost("")]
-        public async Task<ActivitySignUpForm> Create([FromBody]ActivitySignUpForm form)
+        public async Task<ActivitySignUpForm> Create([FromBody] ActivitySignUpForm form)
         {
-           return await _repository.Create(form);
+            var validator = new ActivitySignUpFormValidator();
+            var result = await validator.ValidateAsync(form);
+            if (!result.IsValid) throw new Exception(string.Join(",", result.Errors));
+            return await _repository.Create(form);
         }
 
         [HttpPut("{id}")]
-        public async Task<bool> Update([FromRoute]int id, [FromBody] ActivitySignUpForm form)
+        public async Task<bool> Update([FromRoute] int id, [FromBody] ActivitySignUpForm form)
         {
+            var validator = new ActivitySignUpFormValidator();
+            var result = await validator.ValidateAsync(form);
+            if (!result.IsValid) throw new Exception(string.Join(",",result.Errors));
             return await _repository.Update(id, form);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete([FromRoute] int id)
+        {
+              await _repository.Delete(id);
         }
     }
 }
---
+++ Validation/ActivitySignUpFormValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcmeWidgetAPI.Validation
+{
+    public class ActivitySignUpFormValidator : AbstractValidator<ActivitySignUpForm>
+    {
+        public ActivitySignUpFormValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required.");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name is required.");
+            RuleFor(x => x.EmailAddress).NotEmpty().WithMessage("Email address is required.");
+            RuleFor(x => x.EmailAddress).EmailAddress().WithMessage("Email address should be valid.");
+            RuleFor(x => x.Activity).NotEmpty().WithMessage("Activity must be specified");
+            RuleFor(x => x.Activity).IsInEnum().WithMessage("Activity must be according to the specifications.");
+        }
+    }
+}
